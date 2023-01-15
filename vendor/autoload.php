8b124dbbf62fe6fcf9fc6ea1619d9784eee62b3a
shei25420 <chegeeric66@gmail.com>
2023-01-16 00:09:24 +0300

    composer update with php 8.0.2

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitd4fb2ffa4e831ed9cee758c8301df17a::getLoader();
+return ComposerAutoloaderInit4140f029b25f67d3cba4fd2f001bfe10::getLoader();
